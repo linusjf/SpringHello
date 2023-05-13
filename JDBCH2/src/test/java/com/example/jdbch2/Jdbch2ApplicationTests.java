ac4a3f55d3c3bfd07b0f280d3c7be65e405abd88
Linus Fernandes <linus.fernandes@gmail.com>
2023-05-13 16:28:07 +0530

    feat: add jdbc and h2 example

---
+++ JDBCH2/src/test/java/com/example/jdbch2/Jdbch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdbch2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
