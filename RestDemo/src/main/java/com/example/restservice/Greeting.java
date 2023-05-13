d36c3dc208d0693a6c2a5e742a89b7808d97961b
Linus Fernandes <linus.fernandes@gmail.com>
2023-05-13 05:50:20 +0530

    feat: add RestService example

---
+++ RestDemo/src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,8 @@
+/**
+ * @author      : Linus Fernandes (linusfernandes at gmail dot com)
+ * @file        : Greeting
+ * @created     : Saturday May 13, 2023 05:03:30 IST
+ */
+package com.example.restservice;
+public record Greeting(long id, String content){
+}
